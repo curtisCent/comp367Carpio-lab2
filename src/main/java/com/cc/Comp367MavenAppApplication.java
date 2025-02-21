c373a8e04964d042e7ec8620b29c041f3c913f72
curtisCent <ccarpio2@my.centennialcollege.ca>
2025-02-21 14:16:22 -0500

    added code

---
+++ src/main/java/com/cc/Comp367MavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367MavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp367MavenAppApplication.class, args);
+	}
+
+}
